flutter/flutter

commit 8f0a2ee0e5b286df89c0182a41da3b272e2c1956

[Windows] Don't always stop engine on view destruction (#51681)
Currently destroying a view also shuts down the engine. This makes sense as in single-view world where the view also always owns the engine. However, in a multi-view world the views will share the engine. Destroying one view shouldn't necessarily shut down the engine unless that view owns the engine.

Part of https://github.com/flutter/flutter/issues/142845

[C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

shell/platform/windows/flutter_windows_view.cc
@@ -105,10 +105,6 @@ FlutterWindowsView::~FlutterWindowsView() {
   // Notify the engine the view's child window will no longer be visible.
   engine_->OnWindowStateEvent(GetWindowHandle(), WindowStateEvent::kHide);
 
-  // The engine renders into the view's surface. The engine must be
-  // shutdown before the view's resources can be destroyed.
-  engine_->Stop();
-
   DestroyRenderSurface();
 }
 

shell/platform/windows/flutter_windows_view_unittests.cc
@@ -18,6 +18,7 @@
 #include "flutter/shell/platform/windows/flutter_window.h"
 #include "flutter/shell/platform/windows/flutter_windows_engine.h"
 #include "flutter/shell/platform/windows/flutter_windows_texture_registrar.h"
+#include "flutter/shell/platform/windows/flutter_windows_view_controller.h"
 #include "flutter/shell/platform/windows/testing/egl/mock_context.h"
 #include "flutter/shell/platform/windows/testing/egl/mock_manager.h"
 #include "flutter/shell/platform/windows/testing/egl/mock_window_surface.h"
@@ -121,6 +122,7 @@ class MockFlutterWindowsEngine : public FlutterWindowsEngine {
 
   MOCK_METHOD(bool, running, (), (const));
   MOCK_METHOD(bool, Stop, (), ());
+  MOCK_METHOD(void, RemoveView, (FlutterViewId view_id), ());
   MOCK_METHOD(bool, PostRasterThreadTask, (fml::closure), (const));
 
  private:
@@ -241,6 +243,8 @@ TEST(FlutterWindowsViewTest, Shutdown) {
       std::make_unique<NiceMock<MockWindowBindingHandler>>();
   auto egl_manager = std::make_unique<egl::MockManager>();
   auto surface = std::make_unique<egl::MockWindowSurface>();
+
+  auto engine_ptr = engine.get();
   auto surface_ptr = surface.get();
 
   EngineModifier modifier{engine.get()};
@@ -250,12 +254,16 @@ TEST(FlutterWindowsViewTest, Shutdown) {
     std::unique_ptr<FlutterWindowsView> view =
         engine->CreateView(std::move(window_binding_handler));
 
+    auto view_id = view->view_id();
     ViewModifier view_modifier{view.get()};
     view_modifier.SetSurface(std::move(surface));
 
-    // The engine must be stopped before the surface can be destroyed.
+    FlutterWindowsViewController controller{std::move(engine), std::move(view)};
+
+    // The view must be removed before the surface can be destroyed.
     InSequence s;
-    EXPECT_CALL(*engine.get(), Stop).Times(1);
+    EXPECT_CALL(*engine_ptr, running).WillOnce(Return(true));
+    EXPECT_CALL(*engine_ptr, RemoveView(view_id)).Times(1);
     EXPECT_CALL(*surface_ptr, Destroy).Times(1);
   }
 }
@@ -1392,7 +1400,6 @@ TEST(FlutterWindowsViewTest, DisablesVSyncAtStartup) {
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(false)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
 
-  EXPECT_CALL(*engine.get(), Stop).Times(1);
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};
@@ -1430,7 +1437,6 @@ TEST(FlutterWindowsViewTest, EnablesVSyncAtStartup) {
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(true)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
 
-  EXPECT_CALL(*engine.get(), Stop).Times(1);
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};
@@ -1471,7 +1477,6 @@ TEST(FlutterWindowsViewTest, DisablesVSyncAfterStartup) {
   EXPECT_CALL(*surface_ptr, MakeCurrent).WillOnce(Return(true));
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(false)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
-  EXPECT_CALL(*engine.get(), Stop).Times(1);
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};
@@ -1514,7 +1519,6 @@ TEST(FlutterWindowsViewTest, EnablesVSyncAfterStartup) {
   EXPECT_CALL(*surface_ptr, MakeCurrent).WillOnce(Return(true));
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(true)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
-  EXPECT_CALL(*engine.get(), Stop).Times(1);
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};
@@ -1563,7 +1567,6 @@ TEST(FlutterWindowsViewTest, UpdatesVSyncOnDwmUpdates) {
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(false)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
 
-  EXPECT_CALL(*engine.get(), Stop).Times(1);
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier engine_modifier{engine.get()};

shell/platform/windows/BUILD.gn
@@ -87,6 +87,7 @@ source_set("flutter_windows_source") {
     "flutter_windows_texture_registrar.h",
     "flutter_windows_view.cc",
     "flutter_windows_view.h",
+    "flutter_windows_view_controller.cc",
     "flutter_windows_view_controller.h",
     "keyboard_handler_base.h",
     "keyboard_key_channel_handler.cc",

shell/platform/windows/flutter_windows.cc
@@ -130,6 +130,7 @@ FlutterDesktopViewControllerRef FlutterDesktopEngineCreateViewController(
 
 void FlutterDesktopViewControllerDestroy(FlutterDesktopViewControllerRef ref) {
   auto controller = ViewControllerFromHandle(ref);
+  controller->Destroy();
   delete controller;
 }
 

shell/platform/windows/flutter_windows_engine.cc
@@ -502,6 +502,24 @@ std::unique_ptr<FlutterWindowsView> FlutterWindowsEngine::CreateView(
   return std::move(view);
 }
 
+void FlutterWindowsEngine::RemoveView(FlutterViewId view_id) {
+  FML_DCHECK(running());
+  FML_DCHECK(views_.find(view_id) != views_.end());
+
+  if (view_id == kImplicitViewId) {
+    // The engine and framework assume the implicit view always exists.
+    // Attempts to render to the implicit view will be ignored.
+    views_.erase(view_id);
+    return;
+  }
+
+  // TODO(loicsharma): Remove the view from the engine using the
+  // `FlutterEngineRemoveView` embedder API. Windows does not
+  // support views other than the implicit view yet.
+  // https://github.com/flutter/flutter/issues/144810
+  FML_UNREACHABLE();
+}
+
 void FlutterWindowsEngine::OnVsync(intptr_t baton) {
   std::chrono::nanoseconds current_time =
       std::chrono::nanoseconds(embedder_api_.GetCurrentTime());

shell/platform/windows/flutter_windows_engine.h
@@ -124,6 +124,9 @@ class FlutterWindowsEngine {
   std::unique_ptr<FlutterWindowsView> CreateView(
       std::unique_ptr<WindowBindingHandler> window);
 
+  // Remove a view. The engine will no longer render into it.
+  virtual void RemoveView(FlutterViewId view_id);
+
   // Get a view that displays this engine's content.
   //
   // Returns null if the view does not exist.

shell/platform/windows/flutter_windows_unittests.cc
@@ -139,6 +139,9 @@ TEST_F(WindowsTest, EngineCanTransitionToHeadless) {
 
   // The engine is back in headless mode now.
   ASSERT_NE(engine, nullptr);
+
+  auto engine_ptr = reinterpret_cast<FlutterWindowsEngine*>(engine.get());
+  ASSERT_TRUE(engine_ptr->running());
 }
 
 // Verify that accessibility features are initialized when a view is created.

shell/platform/windows/flutter_windows_view_controller.cc
@@ -1,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/windows/flutter_windows_view_controller.h"
+
+namespace flutter {
+
+FlutterWindowsViewController::~FlutterWindowsViewController() {
+  Destroy();
+}
+
+void FlutterWindowsViewController::Destroy() {
+  if (!view_) {
+    return;
+  }
+
+  // Prevent the engine from rendering into this view.
+  if (view_->GetEngine()->running()) {
+    auto view_id = view_->view_id();
+
+    view_->GetEngine()->RemoveView(view_id);
+  }
+
+  // Destroy the view, followed by the engine if it is owned by this controller.
+  view_.reset();
+  engine_.reset();
+}
+
+}  // namespace flutter

bmdcoorrine/engine

commit 01aff04de0109b94e1887634e1f2b386ca88918e

[Windows] Fix EGL surface destruction race (#51781)
This fixes the `WindowsTest.EngineCanTransitionToHeadless` flakiness reported by @matanlurey.

EGL surfaces can only be used by a single thread at a time. Concurrent operations are unsafe.

Previously, the EGL surface was destroyed on the platform thread. This was safe as this always happened after the engine was shutdown and the raster thread was stopped. However, in a multi-view world a view can be destroyed while the engine is running. There may be pending raster tasks that operate on the render surface. Thus, the EGL surfaces should be destroyed on the raster thread when it is available.

This bug was introduced by https://github.com/flutter/engine/pull/51681
Part of https://github.com/flutter/flutter/issues/142845

[C++, Objective-C, Java style guides]: https://github.com/flutter/engine/blob/main/CONTRIBUTING.md#style

shell/platform/windows/flutter_windows_view.cc
@@ -7,7 +7,9 @@
 #include <chrono>
 
 #include "flutter/common/constants.h"
+#include "flutter/fml/make_copyable.h"
 #include "flutter/fml/platform/win/wstring_conversion.h"
+#include "flutter/fml/synchronization/waitable_event.h"
 #include "flutter/shell/platform/common/accessibility_bridge.h"
 #include "flutter/shell/platform/windows/keyboard_key_channel_handler.h"
 #include "flutter/shell/platform/windows/text_input_plugin.h"
@@ -81,6 +83,22 @@ void UpdateVsync(const FlutterWindowsEngine& engine,
   }
 }
 
+/// Destroys a rendering surface that backs a Flutter view.
+void DestroyWindowSurface(const FlutterWindowsEngine& engine,
+                          std::unique_ptr<egl::WindowSurface> surface) {
+  // EGL surfaces are used on the raster thread if the engine is running.
+  // There may be pending raster tasks that use this surface. Destroy the
+  // surface on the raster thread to avoid concurrent uses.
+  if (engine.running()) {
+    engine.PostRasterThreadTask(fml::MakeCopyable(
+        [surface = std::move(surface)] { surface->Destroy(); }));
+  } else {
+    // There's no raster thread if engine isn't running. The surface can be
+    // destroyed on the platform thread.
+    surface->Destroy();
+  }
+}
+
 }  // namespace
 
 FlutterWindowsView::FlutterWindowsView(
@@ -105,7 +123,9 @@ FlutterWindowsView::~FlutterWindowsView() {
   // Notify the engine the view's child window will no longer be visible.
   engine_->OnWindowStateEvent(GetWindowHandle(), WindowStateEvent::kHide);
 
-  DestroyRenderSurface();
+  if (surface_) {
+    DestroyWindowSurface(*engine_, std::move(surface_));
+  }
 }
 
 bool FlutterWindowsView::OnEmptyFrameGenerated() {
@@ -704,12 +724,6 @@ bool FlutterWindowsView::ResizeRenderSurface(size_t width, size_t height) {
 
   surface_ = std::move(resized_surface);
   return true;
-}
-
-void FlutterWindowsView::DestroyRenderSurface() {
-  if (surface_) {
-    surface_->Destroy();
-  }
 }
 
 egl::WindowSurface* FlutterWindowsView::surface() const {

shell/platform/windows/flutter_windows_view.h
@@ -50,9 +50,6 @@ class FlutterWindowsView : public WindowBindingHandlerDelegate {
   // Should be called before calling FlutterEngineRun using this view.
   void CreateRenderSurface();
 
-  // Destroys current rendering surface if one has been allocated.
-  void DestroyRenderSurface();
-
   // Get the EGL surface that backs the Flutter view.
   //
   // This might be nullptr or an invalid surface.

shell/platform/windows/flutter_windows_view_unittests.cc
@@ -264,6 +264,12 @@ TEST(FlutterWindowsViewTest, Shutdown) {
     InSequence s;
     EXPECT_CALL(*engine_ptr, running).WillOnce(Return(true));
     EXPECT_CALL(*engine_ptr, RemoveView(view_id)).Times(1);
+    EXPECT_CALL(*engine_ptr, running).WillOnce(Return(true));
+    EXPECT_CALL(*engine_ptr, PostRasterThreadTask)
+        .WillOnce([](fml::closure callback) {
+          callback();
+          return true;
+        });
     EXPECT_CALL(*surface_ptr, Destroy).Times(1);
   }
 }
@@ -825,7 +831,14 @@ TEST(FlutterWindowsViewTest, WindowResizeTests) {
   auto windows_proc_table = std::make_shared<NiceMock<MockWindowsProcTable>>();
   std::unique_ptr<FlutterWindowsEngine> engine =
       GetTestEngine(windows_proc_table);
+
   EngineModifier engine_modifier{engine.get()};
+  engine_modifier.embedder_api().PostRenderThreadTask = MOCK_ENGINE_PROC(
+      PostRenderThreadTask,
+      ([](auto engine, VoidCallback callback, void* user_data) {
+        callback(user_data);
+        return kSuccess;
+      }));
 
   auto egl_manager = std::make_unique<egl::MockManager>();
   auto surface = std::make_unique<egl::MockWindowSurface>();
@@ -883,7 +896,14 @@ TEST(FlutterWindowsViewTest, TestEmptyFrameResizes) {
   auto windows_proc_table = std::make_shared<NiceMock<MockWindowsProcTable>>();
   std::unique_ptr<FlutterWindowsEngine> engine =
       GetTestEngine(windows_proc_table);
+
   EngineModifier engine_modifier{engine.get()};
+  engine_modifier.embedder_api().PostRenderThreadTask = MOCK_ENGINE_PROC(
+      PostRenderThreadTask,
+      ([](auto engine, VoidCallback callback, void* user_data) {
+        callback(user_data);
+        return kSuccess;
+      }));
 
   auto egl_manager = std::make_unique<egl::MockManager>();
   auto surface = std::make_unique<egl::MockWindowSurface>();
@@ -940,7 +960,14 @@ TEST(FlutterWindowsViewTest, TestEmptyFrameResizes) {
 // https://github.com/flutter/flutter/issues/141855
 TEST(FlutterWindowsViewTest, WindowResizeRace) {
   std::unique_ptr<FlutterWindowsEngine> engine = GetTestEngine();
+
   EngineModifier engine_modifier(engine.get());
+  engine_modifier.embedder_api().PostRenderThreadTask = MOCK_ENGINE_PROC(
+      PostRenderThreadTask,
+      ([](auto engine, VoidCallback callback, void* user_data) {
+        callback(user_data);
+        return kSuccess;
+      }));
 
   auto egl_manager = std::make_unique<egl::MockManager>();
   auto surface = std::make_unique<egl::MockWindowSurface>();
@@ -980,7 +1007,14 @@ TEST(FlutterWindowsViewTest, WindowResizeRace) {
 // even though EGL initialized successfully.
 TEST(FlutterWindowsViewTest, WindowResizeInvalidSurface) {
   std::unique_ptr<FlutterWindowsEngine> engine = GetTestEngine();
+
   EngineModifier engine_modifier(engine.get());
+  engine_modifier.embedder_api().PostRenderThreadTask = MOCK_ENGINE_PROC(
+      PostRenderThreadTask,
+      ([](auto engine, VoidCallback callback, void* user_data) {
+        callback(user_data);
+        return kSuccess;
+      }));
 
   auto egl_manager = std::make_unique<egl::MockManager>();
   auto surface = std::make_unique<egl::MockWindowSurface>();
@@ -1477,6 +1511,11 @@ TEST(FlutterWindowsViewTest, DisablesVSyncAfterStartup) {
   EXPECT_CALL(*surface_ptr, MakeCurrent).WillOnce(Return(true));
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(false)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
+  EXPECT_CALL(*engine.get(), PostRasterThreadTask)
+      .WillOnce([](fml::closure callback) {
+        callback();
+        return true;
+      });
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};
@@ -1519,6 +1558,12 @@ TEST(FlutterWindowsViewTest, EnablesVSyncAfterStartup) {
   EXPECT_CALL(*surface_ptr, MakeCurrent).WillOnce(Return(true));
   EXPECT_CALL(*surface_ptr, SetVSyncEnabled(true)).WillOnce(Return(true));
   EXPECT_CALL(render_context, ClearCurrent).WillOnce(Return(true));
+
+  EXPECT_CALL(*engine.get(), PostRasterThreadTask)
+      .WillOnce([](fml::closure callback) {
+        callback();
+        return true;
+      });
   EXPECT_CALL(*surface_ptr, Destroy).Times(1);
 
   EngineModifier modifier{engine.get()};